5c25cb1a307d5f3a20b58a3c05aa0f85a31a97d1
KimBH04 <102945597+KimBH04@users.noreply.github.com>
2023-05-17 09:28:20 +0900

    [Silver V] Title: 최소공배수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+typedef long long int ll;
+
+ll GCD(ll a, ll b)
+{
+    while (b)
+    {
+        ll r = a % b;
+        a = b;
+        b = r;
+    }
+
+    return a;
+}
+
+int main()
+{
+    ll a, b;
+    scanf("%lld %lld", &a, &b);
+    if (a < b)
+    {
+        ll temp = a;
+        a = b;
+        b = temp;
+    }
+    
+    printf("%lld", a * b / GCD(a, b));
+    return 0;
+}
