85b6cb425f924f2c263325de28ab740b700890a0
KimBH04 <102945597+KimBH04@users.noreply.github.com>
2023-07-03 11:34:30 +0900

    Update 2×n 타일링 2.cs

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cs
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cs
@@ -1,9 +1,9 @@
-int[] t = new int[1001];
-t[0] = t[1] = 1;
-for (int i = 2; i < 1001; i++)
+int n = int.Parse(Console.ReadLine()!), a = 1, b = 1;
+for (int i = 0; i < n; i++)
 {
-    t[i] = (t[i - 1] + t[i - 2] * 2) % 10007;
+    int t = b;
+    b = (a * 2 + b) % 10007;
+    a = t;
 }
 
-int n = int.Parse(Console.ReadLine()!);
-Console.WriteLine(t[n]);
+Console.WriteLine(a);
