5186f2b66b588395b02632ee00eb7f1b2b377952
KimBH04 <102945597+KimBH04@users.noreply.github.com>
2023-07-19 09:11:33 +0900

    [Platinum I] Title: 삶, 우주, 그리고 모든 것에 관한 궁극적인 질문의 해답, Time: 64 ms, Memory: 5816 KB -BaekjoonHub

---
+++ 백준/Platinum/28123. 삶， 우주， 그리고 모든 것에 관한 궁극적인 질문의 해답/삶， 우주， 그리고 모든 것에 관한 궁극적인 질문의 해답.cs
@@ -0,0 +1,14 @@
+string[] input = Console.ReadLine()!.Split();
+long n = long.Parse(input[0]), k = long.Parse(input[1]), x = long.Parse(input[2]);
+long r = 0;
+
+while (x > 1)
+{
+    if (x == 4)
+        r++;
+    x /= 2;
+    n--;
+}
+
+r += n - 3 * (k - 1);
+Console.WriteLine(r);
