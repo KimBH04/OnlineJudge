0bd09047e802b91e09b8bd80491b17fef1834392
KimBH04 <102945597+KimBH04@users.noreply.github.com>
2023-07-26 19:22:40 +0900

    [level 0] Title: 문자열 뒤집기, Time: 1.49 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public string solution(string my_string) {
+        string answer = "";
+        foreach (char c in my_string.Reverse()) {
+            answer += c;
+        }
+        return answer;
+    }
+}
