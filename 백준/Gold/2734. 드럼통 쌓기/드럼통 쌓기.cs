eeecb1f37806538354a1067ada550600174e13cc
KimBH04 <102945597+KimBH04@users.noreply.github.com>
2023-07-27 09:08:52 +0900

    [Gold IV] Title: 드럼통 쌓기, Time: 80 ms, Memory: 6404 KB -BaekjoonHub

---
+++ 백준/Gold/2734. 드럼통 쌓기/드럼통 쌓기.cs
@@ -0,0 +1,67 @@
+using static System.Math;
+
+int t = int.Parse(Console.ReadLine()!);
+while (t-- > 0)
+{
+    string[] input = Console.ReadLine()!.Split();
+    int n = int.Parse(input[0]);
+
+    Circle[] circles = new Circle[n];
+    for (int i = 0; i < n; i++)
+    {
+        circles[i] = new(double.Parse(input[i + 1]), 1);
+    }
+
+    while (n-- > 1)
+    {
+        Circle[] onCircles = new Circle[n];
+        for (int i = 0; i < n; i++)
+        {
+            onCircles[i] = Circle.BetweenOn(circles[i], circles[i + 1]);
+        }
+        circles = onCircles.ToArray();
+    }
+
+    Console.WriteLine($"{circles[0].X:0.0000} {circles[0].Y:0.0000}");
+}
+
+public readonly struct Circle
+{
+    private readonly double x;
+    private readonly double y;
+
+    public Circle(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double X => x;
+    public double Y => y;
+
+    public static Circle BetweenOn(Circle a, Circle b)
+    {
+        double x1 = a.X;
+        double y1 = a.Y;
+        double x2 = b.X;
+        double y2 = b.Y;
+
+        double radian = Atan2(y2 - y1, x2 - x1);
+
+        double halfx = (x1 + x2) / 2;
+        double halfy = (y1 + y2) / 2;
+
+        double halfDisx = halfx - x1;
+        double halfDisy = halfy - y1;
+
+        double halfDis = halfDisx * halfDisx + halfDisy * halfDisy;
+
+        double resultDis = Sqrt(4 - halfDis);
+
+        //C#은 삼각함수에 각도가 아닌 라디안을 넣어야 했던 건에 대하여...
+        double resultx = Sin(radian) * resultDis;
+        double resulty = Cos(radian) * resultDis;
+
+        return new Circle(halfx - resultx, halfy + resulty);
+    }
+}
