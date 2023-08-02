b675589476aaf1f95b4a2e9edcaea8cdc84116aa
KimBH04 <102945597+KimBH04@users.noreply.github.com>
2023-08-02 17:01:24 +0900

    [Unrated] Title: Binary Game 2, Time: 208 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Unrated/24901. Binary Game 2/Binary Game 2.cc
+++ 백준/Unrated/24901. Binary Game 2/Binary Game 2.cc
@@ -1,31 +1,19 @@
+/* /* */
+//#\u000a/*
+#include <iostream>/*
+b = [0, 1, 10, 11, 100, 101, 110, 111, 1000, 1001, 1010]
+n = gets.to_i
+for i in 0..n
+  print b.at(i)
+end
+=begin
+*/
+//\u000aimport java.util.*; public class Main { public static void main(String[] args) {int[] b = {0, 1, 10, 11, 100, 101, 110, 111, 1000, 1001, 1010}; Scanner scanner = new Scanner(System.in); int n = scanner.nextInt(); int i = 0; while (i <= n) {System.out.print(b[i++]);} scanner.close();}}
 //\u000a/*
-
-#include <iostream>
-
-int main()
-{
-    int b[11] = { 0, 1, 10, 11, 100, 101, 110, 111, 1000, 1001, 1010 }, n;
-    std::cin >> n;
-    for (int i = 0; i <= n; i++)
-    {
-        std::cout << b[i];
-    }
-}
-//*/
-/*\u002a/
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        int[] b = { 0, 1, 10, 11, 100, 101, 110, 111, 1000, 1001, 1010 };
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        for (int i = 0; i <= n; i++) {
-            System.out.print(b[i]);
-        }
-        scanner.close();
-    }
-}
-
-/*\u002a/
-// */
+void main(){int b[11] = {0, 1, 10, 11, 100, 101, 110, 111, 1000, 1001, 1010}, n, i = 0;std::cin >> n;while (i <= n){std::cout << b[i++];}}
+//#\u000a*/
+//#*/*/for i in 0...Int(readLine()!)!{print(String(i,radix:2),terminator:"")}//\u000a/*
+#if false
+=end
+#endif
+//#\u000a*/
