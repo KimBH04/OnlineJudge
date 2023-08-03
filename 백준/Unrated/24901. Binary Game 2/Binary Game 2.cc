d43934dcaf965a75035d7468be9083775c33030e
KimBH04 <102945597+KimBH04@users.noreply.github.com>
2023-08-03 10:02:26 +0900

    [Unrated] Title: Binary Game 2, Time: 12 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Unrated/24901. Binary Game 2/Binary Game 2.cc
+++ 백준/Unrated/24901. Binary Game 2/Binary Game 2.cc
@@ -1,19 +1,5 @@
 /* /* */
-//#\u000a/*
-#include <iostream>/*
-b = [0, 1, 10, 11, 100, 101, 110, 111, 1000, 1001, 1010]
-n = gets.to_i
-for i in 0..n
-  print b.at(i)
-end
-=begin
-*/
-//\u000aimport java.util.*; public class Main { public static void main(String[] args) {int[] b = {0, 1, 10, 11, 100, 101, 110, 111, 1000, 1001, 1010}; Scanner scanner = new Scanner(System.in); int n = scanner.nextInt(); int i = 0; while (i <= n) {System.out.print(b[i++]);} scanner.close();}}
-//\u000a/*
-void main(){int b[11] = {0, 1, 10, 11, 100, 101, 110, 111, 1000, 1001, 1010}, n, i = 0;std::cin >> n;while (i <= n){std::cout << b[i++];}}
-//#\u000a*/
-//#*/*/for i in 0...Int(readLine()!)!{print(String(i,radix:2),terminator:"")}//\u000a/*
-#if false
-=end
-#endif
-//#\u000a*/
+#include <iostream>
+using namespace std;
+int main(){int n,b[11]={0,1,10,11,100,101,110,111,1000,1001,1010};cin>>n;for(int i=0;i<=n;i++)cout<<b[i];return 0;}
+// */for i in 0...Int(readLine()!)!{print(String(i, radix:2),terminator:"")}
