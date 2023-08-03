063ad2af65d53cf9fd8522ecab740d1e7dd7c6e1
KimBH04 <102945597+KimBH04@users.noreply.github.com>
2023-08-03 10:35:25 +0900

    [Unrated] Title: Binary Game 2, Time: 208 ms, Memory: 98404 KB -BaekjoonHub

---
+++ 백준/Unrated/24901. Binary Game 2/Binary Game 2.c
@@ -0,0 +1,27 @@
+//#\u000a/*
+#if true /*
+=begin
+*/
+// \u000a/*
+#endif
+// #\u000a*/
+/* /* */ //\u000a/*
+#include <stdio.h>//\u000a*/
+/*
+=end
+b = [0, 1, 10, 11, 100, 101, 110, 111, 1000, 1001, 1010]
+n = gets.to_i
+for i in 0..n
+  print b.at(i)
+end
+=begin
+*/
+//\u000aimport java.util.*;public class Main{public static void main(String[]a){Scanner s=new Scanner(System.in);int[]b={0,1,10,11,100,101,110,111,1000,1001,1010};int n=s.nextInt();s.close();for(int i=0;i<=n;i++){System.out.print(b[i]);}}}/*
+int main(){int n,b[11]={0,1,10,11,100,101,110,111,1000,1001,1010};scanf("%d",&n);for(int i=0;i<=n;i++)printf("%d",b[i]);return 0;}// */
+// */for i in 0...Int(readLine()!)!{print(String(i, radix:2),terminator:"")}//\u000a/*
+#if true
+/*
+=end
+#*/ // \u000a/*
+#endif
+//#\u000a*/
