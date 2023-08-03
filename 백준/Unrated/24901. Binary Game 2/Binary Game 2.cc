b053d22b190ae1632d253152a03275fee6d34a98
KimBH04 <102945597+KimBH04@users.noreply.github.com>
2023-08-03 10:29:30 +0900

    Update Binary Game 2.cc

--- 백준/Unrated/24901. Binary Game 2/Binary Game 2.cc
+++ 백준/Unrated/24901. Binary Game 2/Binary Game 2.cc
@@ -1,5 +1,28 @@
-/* /* */
-#include <iostream>
+//#\u000a/*
+#if true /*
+=begin
+*/
+// \u000a/*
+#endif
+// #\u000a*/
+/* /* */ //\u000a/*
+#include <iostream>//\u000a*/
+/*
+=end
+b = [0, 1, 10, 11, 100, 101, 110, 111, 1000, 1001, 1010]
+n = gets.to_i
+for i in 0..n
+  print b.at(i)
+end
+=begin
+*/
+//\u000aimport java.util.*;public class Main{public static void main(String[]a){Scanner s=new Scanner(System.in);int[]b={0,1,10,11,100,101,110,111,1000,1001,1010};int n=s.nextInt();s.close();for(int i=0;i<=n;i++){System.out.print(b[i]);}}}/*
 using namespace std;
-int main(){int n,b[11]={0,1,10,11,100,101,110,111,1000,1001,1010};cin>>n;for(int i=0;i<=n;i++)cout<<b[i];return 0;}
-// */for i in 0...Int(readLine()!)!{print(String(i, radix:2),terminator:"")}
+int main(){int n,b[11]={0,1,10,11,100,101,110,111,1000,1001,1010};cin>>n;for(int i=0;i<=n;i++)cout<<b[i];return 0;}// */
+// */for i in 0...Int(readLine()!)!{print(String(i, radix:2),terminator:"")}//\u000a/*
+#if true
+/*
+=end
+#*/ // \u000a/*
+#endif
+//#\u000a*/
