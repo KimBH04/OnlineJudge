279c69803b6476e139d2002e0af0424106eab120
KimBH04 <102945597+KimBH04@users.noreply.github.com>
2023-08-11 16:04:04 +0900

    [Silver III] Title: 행렬 연산 (행렬 계산하기), Time: 768 ms, Memory: 24484 KB -BaekjoonHub

---
+++ 백준/Silver/28438. 행렬 연산 （행렬 계산하기）/행렬 연산 （행렬 계산하기）.cs
@@ -0,0 +1,36 @@
+using StreamReader sr = new(Console.OpenStandardInput());
+using StreamWriter sw = new(Console.OpenStandardOutput());
+
+string[] input = sr.ReadLine()!.Split();
+int n = int.Parse(input[0]),
+    m = int.Parse(input[1]),
+    q = int.Parse(input[2]);
+
+long[] ro = Enumerable.Repeat(0L, n).ToArray(),
+       cu = Enumerable.Repeat(0L, m).ToArray();
+
+while (q-- > 0)
+{
+    input = sr.ReadLine()!.Split();
+    int a = int.Parse(input[0]),
+        b = int.Parse(input[1]) - 1,
+        c = int.Parse(input[2]);
+
+    if (a < 2)
+    {
+        ro[b] += c;
+    }
+    else
+    {
+        cu[b] += c;
+    }
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        sw.Write($"{ro[i] + cu[j]} ");
+    }
+    sw.WriteLine();
+}
