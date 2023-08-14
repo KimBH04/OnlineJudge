2ffabb1cfd2763cabf884f20644bb744f759b527
KimBH04 <102945597+KimBH04@users.noreply.github.com>
2023-08-14 10:49:24 +0900

    [Bronze V] Title: Serca, Time: 520 ms, Memory: 6584 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cs
@@ -0,0 +1,6 @@
+using StreamWriter sw = new(Console.OpenStandardOutput());
+int n = int.Parse(Console.ReadLine()!);
+while (n-- > 0)
+{
+    sw.WriteLine(" @@@   @@@ \n@   @ @   @\n@    @    @\n@         @\n @       @ \n  @     @  \n   @   @   \n    @ @    \n     @     ");
+}
