03ef00cf27b1cdedc90f5ba75f5aaf7a69e35f26
KimBH04 <102945597+KimBH04@users.noreply.github.com>
2023-08-14 10:50:06 +0900

    [Bronze V] Title: Serca, Time: 120 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    while (n--)
+    {
+        printf(" @@@   @@@ \n@   @ @   @\n@    @    @\n@         @\n @       @ \n  @     @  \n   @   @   \n    @ @    \n     @     \n");
+    }
+}
