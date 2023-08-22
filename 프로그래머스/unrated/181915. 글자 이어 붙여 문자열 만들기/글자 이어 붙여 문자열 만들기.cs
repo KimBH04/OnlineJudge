1c6fc77e7b299ae037454801c4a73528c71fd103
KimBH04 <102945597+KimBH04@users.noreply.github.com>
2023-08-22 19:48:23 +0900

    [unrated] Title: 글자 이어 붙여 문자열 만들기, Time: 0.25 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, int[] index_list) {
+        string answer = "";
+        foreach (int i in index_list) {
+            answer += my_string[i];
+        }
+        return answer;
+    }
+}
