e6662cae2182819a976908920d88b5aaea60f47c
KBH <102945597+KimBH04@users.noreply.github.com>
2023-09-15 14:43:02 +0900

    [Unrated] Title: Rick, Time: 60 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Unrated/15639. Rick/Rick.cs
@@ -0,0 +1,14 @@
+string rick = Console.ReadLine()!;
+if (rick == "Give you up" ||
+    rick == "Let you down" ||
+    rick == "Run around and desert you" ||
+    rick == "Make you cry" ||
+    rick == "Say goodbye" ||
+    rick == "Tell a lie and hurt you")
+{
+    Console.WriteLine("NO");
+}
+else
+{
+    Console.WriteLine("YES");
+}
