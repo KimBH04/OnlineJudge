faf1397ede2f251877bfe8ca0799d7e02d8395de
KimBH <102945597+KimBH04@users.noreply.github.com>
2023-10-20 21:15:06 +0900

    [Silver III] Title: N과 M (2), Time: 64 ms, Memory: 5528 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cs
@@ -0,0 +1,21 @@
+string[] inputs = Console.ReadLine()!.Split();
+int n = int.Parse(inputs[0]),
+    m = int.Parse(inputs[1]);
+
+BackTracking(new int[m], 1, n, 0, m);
+
+static void BackTracking(int[] arr, int start, int end, int m, int depth)
+{
+    if (m == depth)
+    {
+        Console.WriteLine(string.Join(' ', arr));
+    }
+    else
+    {
+        for (int i = start; i <= end; i++)
+        {
+            arr[m] = i;
+            BackTracking(arr, i + 1, end, m + 1, depth);
+        }
+    }
+}
