8d739946658be6baa863ed9f0da230e3e65f7756
KimBH <102945597+KimBH04@users.noreply.github.com>
2023-10-22 16:32:56 +0900

    [Silver III] Title: N과 M (6), Time: 68 ms, Memory: 6128 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cs
@@ -0,0 +1,30 @@
+using StreamWriter sw = new(Console.OpenStandardOutput());
+
+string[] inputs = Console.ReadLine()!.Split();
+int n = int.Parse(inputs[0]), m = int.Parse(inputs[1]);
+
+inputs = Console.ReadLine()!.Split();
+int[] nums = new int[n];
+for (int i = 0; i < n; i++)
+{
+    nums[i] = int.Parse(inputs[i]);
+}
+
+Array.Sort(nums);
+BackTracking(sw, nums, new int[m], 0, n, 0, m);
+
+static void BackTracking(StreamWriter sw, int[] nums, int[] arr, int start, int end, int m, int depth)
+{
+    if (m == depth)
+    {
+        sw.WriteLine(string.Join(' ', arr));
+    }
+    else
+    {
+        for (int i = start; i < end; i++)
+        {
+            arr[m] = nums[i];
+            BackTracking(sw, nums, arr, i + 1, end, m + 1, depth);
+        }
+    }
+}
