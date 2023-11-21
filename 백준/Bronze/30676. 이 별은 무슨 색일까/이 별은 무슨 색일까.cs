fbf56df5343069aa66532fe349601395cab91cdc
KimBH <102945597+KimBH04@users.noreply.github.com>
2023-11-21 21:50:11 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 60 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.cs
@@ -0,0 +1,33 @@
+int lambda = int.Parse(Console.ReadLine()!);
+if (lambda < 425)
+{
+    Console.WriteLine("Violet");
+}
+else if (lambda < 450)
+{
+    Console.WriteLine("Indigo");
+}
+else if (lambda < 495)
+{
+    Console.WriteLine("Blue");
+}
+else if (lambda < 570)
+{
+    Console.WriteLine("Green");
+}
+else if (lambda < 590)
+{
+    Console.WriteLine("Yellow");
+}
+else if (lambda < 620)
+{
+    Console.WriteLine("Orange");
+}
+else if (lambda < 781)
+{
+    Console.WriteLine("Red");
+}
+else
+{
+    Console.WriteLine("여기 까지 주어질 리가 없지 ㄹㅇㅋㅋ");
+}
