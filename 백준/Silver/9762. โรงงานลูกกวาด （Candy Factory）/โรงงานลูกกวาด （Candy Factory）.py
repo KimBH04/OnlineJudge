58189b29b9ddef8e684703f99eee8868627158da
KimBH <102945597+KimBH04@users.noreply.github.com>
2024-01-03 13:38:54 +0900

    [Silver V] Title: โรงงานลูกกวาด (Candy Factory), Time: 120 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/9762. โรงงานลูกกวาด （Candy Factory）/โรงงานลูกกวาด （Candy Factory）.py
+++ 백준/Silver/9762. โรงงานลูกกวาด （Candy Factory）/โรงงานลูกกวาด （Candy Factory）.py
@@ -1,7 +1,7 @@
 H=0
 def S(a,n):
  global H
- if a[n]==-1:l,r=S(a,n*2),S(a,n*2+1);a[n]=min(l,r);H+=abs(l-r)
+ if a[n]<1:l,r=S(a,n*2),S(a,n*2+1);a[n]=min(l,r);H+=abs(l-r)
  return a[n]
 t=int(input())
-while t:t-=1;n=int(input());c=(n-n//2)*[-1]+list(map(int,input().split()));print(S(c,1)+H);H=0
+while t:t-=1;n=int(input());c=(n-n//2)*[0]+list(map(int,input().split()));print(S(c,1)+H);H=0
