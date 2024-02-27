f269070c8636ffd091e5b762095c7c79f253cb77
KimBH <102945597+KimBH04@users.noreply.github.com>
2024-02-27 17:40:27 +0900

    [Bronze I] Title: A/B - 2, Time: 88 ms, Memory: 5880 KB, Score: 2000 point -BaekjoonHub

--- 백준/Bronze/15792. A／B － 2/A／B － 2.cs
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.cs
@@ -1,13 +1,29 @@
 using System;
+using System.Numerics;
+using System.Text;
 
-namespace Prob
+namespace Prob15792
 {
     class Program
     {
         static void Main(string[] args)
         {
-            decimal[] input = Array.ConvertAll(Console.ReadLine().Split(), (a) => decimal.Parse(a));
-            Console.WriteLine(input[0] / input[1]);
+            StringBuilder sb = new StringBuilder();
+            BigInteger[] input = Array.ConvertAll(Console.ReadLine().Split(), (a) => BigInteger.Parse(a));
+            sb.Append(input[0] / input[1]);
+            if (input[0] % input[1] == 0)
+                goto here;
+            sb.Append(".");
+            input[0] = input[0] % input[1] * 10;
+            for (int i = 0; i < 1000; i++)
+            {
+                sb.Append(input[0] / input[1]);
+                if (input[0] % input[1] == 0)
+                    goto here;
+                input[0] = input[0] % input[1] * 10;
+            }
+        here:
+            Console.WriteLine(sb);
         }
     }
 }
