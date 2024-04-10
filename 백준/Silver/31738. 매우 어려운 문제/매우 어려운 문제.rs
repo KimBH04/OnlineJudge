75057638067d987240058139b2289b5b6114ab34
KimBH <102945597+KimBH04@users.noreply.github.com>
2024-04-10 11:39:49 +0900

    [Silver V] Title: 매우 어려운 문제, Time: 120 ms, Memory: 13204 KB -BaekjoonHub

---
+++ 백준/Silver/31738. 매우 어려운 문제/매우 어려운 문제.rs
@@ -0,0 +1,19 @@
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut buf = String::new();
+    stdin().read_to_string(&mut buf).unwrap();
+    let mut input = buf.split_whitespace().flat_map(str::parse::<usize>);
+    
+    let n = input.next().unwrap();
+    let m = input.next().unwrap();
+    if n >= m {
+        println!("0");
+    } else {
+        let mut a = 1;
+        for i in 2..=n {
+            a = a % m * i % m;
+        }
+        println!("{a}");
+    }
+}
