abdb084985baefa25bd8019f652905c98d9490f7
KimBH <102945597+KimBH04@users.noreply.github.com>
2024-05-09 12:00:07 +0900

    [Bronze III] Title: 단원평가, Time: 0 ms, Memory: 1128 KB -BaekjoonHub

--- 백준/Bronze/31798. 단원평가/단원평가.c
+++ 백준/Bronze/31798. 단원평가/단원평가.c
@@ -5,6 +5,6 @@ int main() {
     int a, b, c, d;
     scanf("%d%d%d", &a, &b, &c);
     d = a + b;
-    printf("%d", c ? c * c - (d) : (int)(sqrt(d) + 0.5));
+    printf("%d", c ? c * c - d : (int)(sqrt(d) + 0.5));
     return 0;
 }
