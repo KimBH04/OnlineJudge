5d95bc58bdca758542c99e8e99e39a03835d625e
KimBH <102945597+KimBH04@users.noreply.github.com>
2024-05-28 10:53:50 +0900

    [Gold IV] Title: 다항식 게임, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11560. 다항식 게임/다항식 게임.py
@@ -0,0 +1,22 @@
+t = int(input())
+while t > 0:
+    k, n = map(int, input().split())
+    dp = [[0] * 256 for _ in range(32)]
+    for i in range(k + 1):
+        for j in range(n + 1):
+            dp[i][j] = 1 if i >= j else 0
+    for i in range(1, k + 1):
+        temp = [0] * 256
+        j = 0
+        while dp[i][j]:
+            l = 0
+            while dp[i - 1][l]:
+                temp[j + l] += dp[i][j] * dp[i - 1][l]
+                l += 1
+            j += 1
+        j = 0
+        while temp[j]:
+            dp[i][j] = temp[j]
+            j += 1
+    print(dp[k][n])
+    t -= 1
