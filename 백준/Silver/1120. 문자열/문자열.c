68fb64f1ed2d93dba18b087b4147ab0c9af25e2b
KimBH <102945597+KimBH04@users.noreply.github.com>
2024-06-04 16:59:47 +0900

    [Silver IV] Title: 문자열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1120. 문자열/문자열.c
+++ 백준/Silver/1120. 문자열/문자열.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-int diff(char *a, char *b, int count);
-
 int main() {
     char a[64], b[64];
     scanf("%s%s", a, b);
@@ -10,17 +8,12 @@ int main() {
     int blen = strlen(b);
     int min = 64;
     for (int i = 0; i <= blen - alen; i++) {
-        int d = diff(a, b + i, alen);
-        min = d < min ? d : min;
+        int diff = 0;
+        for (int j = 0; j < alen; j++) {
+            diff += a[j] != b[i + j];
+        }
+        min = diff < min ? diff : min;
     }
     printf("%d", min);
     return 0;
-}
-
-int diff(char *a, char *b, int count) {
-    int result = 0;
-    for (int i = 0; i < count; i++) {
-        result += a[i] != b[i];
-    }
-    return result;
 }
