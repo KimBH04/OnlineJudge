7a59dbc4b500fea6b4717d1b577067ac2c1178d1
KimBH <102945597+KimBH04@users.noreply.github.com>
2024-06-14 23:00:15 +0900

    [Gold IV] Title: 행렬 제곱, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.c
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.c
@@ -48,7 +48,7 @@ int main() {
     scanf("%d%lld", &n, &b);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            scanf("%d", a[i] + j);;
+            scanf("%d", a[i] + j);
         }
     }
     power(n, b);
