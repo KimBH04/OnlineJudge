b0bd5cad00cb4377a7960e3c22153339a74398ca
KimBH <102945597+KimBH04@users.noreply.github.com>
2024-06-28 14:43:20 +0900

    [Silver III] Title: 꿀 아르바이트, Time: 16 ms, Memory: 1384 KB -BaekjoonHub

--- 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.c
+++ 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.c
@@ -4,14 +4,12 @@ int main() {
     int n, m, t[100000];
     long long int sum = 0L, max = 0L;
     scanf("%d%d", &n, &m);
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < n; i++) {
+        if (i >= m) {
+            if (max < sum) max = sum;
+            sum -= t[i - m];
+        }
         scanf("%d", t + i);
-        sum += t[i];
-    }
-    for (int i = m; i < n; i++) {
-        if (max < sum) max = sum;
-        scanf("%d", t + i);
-        sum -= t[i - m];
         sum += t[i];
     }
     printf("%lld", sum > max ? sum : max);
