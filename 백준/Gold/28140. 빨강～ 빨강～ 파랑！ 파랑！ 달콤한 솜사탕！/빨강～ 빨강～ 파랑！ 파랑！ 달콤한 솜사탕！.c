9ac9ee1cd2600cbec6f761326df073cc5af98711
KimBH <102945597+KimBH04@users.noreply.github.com>
2024-06-29 11:10:30 +0900

    [Gold V] Title: 빨강~ 빨강~ 파랑! 파랑! 달콤한 솜사탕!, Time: 1156 ms, Memory: 9912 KB -BaekjoonHub

---
+++ 백준/Gold/28140. 빨강～ 빨강～ 파랑！ 파랑！ 달콤한 솜사탕！/빨강～ 빨강～ 파랑！ 파랑！ 달콤한 솜사탕！.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static inline size_t binary_search(int *arr, size_t right, int n) {
+    size_t left = 0;
+    while (left < right) {
+        size_t mid = (left + right) / 2;
+        if (arr[mid] < n) {
+            left = mid + 1;
+        }
+        else if (arr[mid] > n) {
+            right = mid;
+        }
+        else {
+            return mid;
+        }
+    }
+    return left;
+}
+
+int main() {
+    int n, q;
+    scanf("%d%d", &n, &q);
+    size_t red_count = 0;
+    size_t blue_count = 0;
+    int *red_indexes = (int*)malloc(sizeof(int) * n);
+    int *blue_indexes = (int*)malloc(sizeof(int) * n);
+    char *s = (char*)malloc(sizeof(char) * (n + 1));
+    scanf("%s", s);
+    for (int i = 0; i < n; i++) {
+        switch (s[i])
+        {
+        case 'R':
+            red_indexes[red_count++] = i;
+            break;
+
+        case 'B':
+            blue_indexes[blue_count++] = i;
+            break;
+        
+        default:
+            break;
+        }
+    }
+    while (q--) {
+        int l, r;
+        scanf("%d%d", &l, &r);
+        if (r - l < 3) {
+            printf("-1\n");
+            continue;
+        }
+        int red = binary_search(red_indexes, red_count, l);
+        int blue = binary_search(blue_indexes, blue_count, r);
+        if (blue == blue_count || blue_indexes[blue] > r) {
+            blue--;
+        }
+        if (blue < 1 || red + 2 > red_count) {
+            printf("-1\n");
+            continue;
+        }
+        int a = red_indexes[red],
+            b = red_indexes[red + 1],
+            c = blue_indexes[blue - 1],
+            d = blue_indexes[blue];
+        if (b < c) {
+            printf("%d %d %d %d\n", a, b, c, d);
+        }
+        else {
+            printf("-1\n");
+        }
+    }
+    free(red_indexes);
+    free(blue_indexes);
+    free(s);
+    return 0;
+}
