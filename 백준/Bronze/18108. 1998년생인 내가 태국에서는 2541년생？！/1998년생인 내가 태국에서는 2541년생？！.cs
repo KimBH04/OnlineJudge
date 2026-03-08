491229d288a37c0f6da386d0e50170c89cf694b1
KimBH <102945597+KimBH04@users.noreply.github.com>
2026-03-08 22:07:13 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 64 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Prob10926
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = Console.ReadLine();
+            int B = int.Parse(A);
+            Console.WriteLine(B - 543);
+        }
+    }
+}
