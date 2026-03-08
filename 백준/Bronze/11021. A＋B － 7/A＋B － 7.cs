bef031f3bd59c0fd026644e75b7149646fad69b7
KimBH <102945597+KimBH04@users.noreply.github.com>
2026-03-08 22:08:05 +0900

    [Bronze V] Title: A+B - 7, Time: 68 ms, Memory: 5852 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Prob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Num = Console.ReadLine();
+            int NoT = int.Parse(Num);
+
+            int[] aW = new int[NoT];
+
+            for (int i = 0; i < NoT; i++)
+            {
+                string b = Console.ReadLine();
+                string[] c = b.Split();
+                int A = int.Parse(c[0]);
+                int B = int.Parse(c[1]);
+
+                aW[i] = A + B;
+            }
+
+            for (int i = 0; i < NoT; i++)
+            {
+                Console.WriteLine("Case #{1}: {0}", aW[i], i + 1);
+            }
+        }
+    }
+}
