c4efdd835b055d5076f0ff7559701d16a520329d
KimBH <102945597+KimBH04@users.noreply.github.com>
2026-03-08 22:11:41 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 64 ms, Memory: 5468 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Prob2743
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] a = Console.ReadLine().ToCharArray();
+            Console.Write(a.Length);
+        }
+    }
+}
