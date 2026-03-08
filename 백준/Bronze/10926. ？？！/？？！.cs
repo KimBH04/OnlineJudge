b45f436d35158776a31aae338a4df1692807d4ee
KimBH <102945597+KimBH04@users.noreply.github.com>
2026-03-08 22:07:11 +0900

    [Bronze V] Title: ??!, Time: 60 ms, Memory: 5360 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Prob10926
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = Console.ReadLine();
+            Console.WriteLine(A + "??!");
+        }
+    }
+}
