ef57c75c0c9dc0240fb8a66b36e06eb77f9db3d6
KimBH <102945597+KimBH04@users.noreply.github.com>
2026-03-08 22:09:52 +0900

    [Bronze V] Title: 아스키 코드, Time: 60 ms, Memory: 5448 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Prob11654
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+            Console.WriteLine(0 + a);
+        }
+    }
+}
